d0f4edf4f5aa300c5731623a620f34c1e09941ea
wasabipesto <21313833+wasabipesto@users.noreply.github.com>
2023-07-08 09:43:35 -0400

    add location

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import psycopg2
 import requests
 from dateutil import parser
 
-
 def update_activity(conn):
     print('Updating table: activity')
 
@@ -33,7 +32,8 @@ def update_activity(conn):
         ).json()
     
     # collate data to submit
-    data_prep = [{'time': parser.isoparse(row['last_changed']), 'state': row['state']} for row in response[0] if row['state'] != 'unknown']
+    acceptable_states = ['still', 'walking', 'running', 'in_vehicle']
+    data_prep = [{'time': parser.isoparse(row['last_changed']), 'state': row['state']} for row in response[0] if row['state'] in acceptable_states]
     data_submit = [{'state':last_state}]
     for row in data_prep:
         if row['state'] != data_submit[-1]['state']:
@@ -44,6 +44,47 @@ def update_activity(conn):
     print('Inserting', len(data_submit), 'rows...')
     for row in data_submit:
         cursor.execute("INSERT INTO activity (time, android_detected_activity) VALUES (%s, %s)",
+            (row["time"], row["state"])
+            )
+    conn.commit()
+    cursor.close()
+
+def update_location(conn):
+    print('Updating table: location')
+
+    # get last row in table
+    cursor = conn.cursor()
+    cursor.execute("SELECT time, hass_detected_zone FROM location ORDER BY time DESC LIMIT 1")
+    result = cursor.fetchone()
+    last_time = result[0]
+    last_state = result[1]
+
+    # get data from api
+    response = requests.get(
+        os.environ.get('HASS_URL')+'/api/history/period/'+last_time.strftime('%Y-%m-%dT%H:%M:%S%z'),
+        headers={
+            'authorization': 'Bearer '+os.environ.get('HASS_API_KEY'),
+            'content-type': 'application/json',
+        },
+        params={
+            'filter_entity_id': 'person.justin',
+            'end_time': datetime.now().strftime('%Y-%m-%dT%H:%M:%S%z'),
+            'no_attributes': True
+        }
+        ).json()
+    
+    # collate data to submit
+    data_prep = [{'time': parser.isoparse(row['last_changed']), 'state': row['state']} for row in response[0]]
+    data_submit = [{'state':last_state}]
+    for row in data_prep:
+        if row['state'] != data_submit[-1]['state']:
+            data_submit.append(row)
+    data_submit.pop(0)
+    
+    # push data to database
+    print('Inserting', len(data_submit), 'rows...')
+    for row in data_submit:
+        cursor.execute("INSERT INTO location (time, hass_detected_zone) VALUES (%s, %s)",
             (row["time"], row["state"])
             )
     conn.commit()
@@ -61,6 +102,7 @@ def main():
 
     # process each table
     update_activity(conn)
+    update_location(conn)
 
     # close the connection
     conn.close()
